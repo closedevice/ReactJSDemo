8cca3667a4ef163c8ef18da205e55cbcd9fac694
sbbic <closedevice@gmail.com>
2016-07-31 22:15:58 +0800

    init

---
+++ build/.module-cache/2440a3fd719d8758934a5a9a00444f2a7cfaa51e.js
@@ -0,0 +1,7 @@
+/**
+ * Created by God on 2016/6/14.
+ */
+eact.render(
+React.createElement("h1", null, "Hello, world!"),
+    document.getElementById('example')
+);
---
+++ test/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by God on 2016/6/14.
+ */
+eact.render(
+<h1>Hello, world!</h1>,
+    document.getElementById('example')
+);
